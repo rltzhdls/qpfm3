b38f42bfe51a7400e7881e3ea087853550e446d5
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-14 15:50:31 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/wjtmd2@main/wjtmd2.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
